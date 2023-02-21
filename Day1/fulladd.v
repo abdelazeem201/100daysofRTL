fb6853ac15b5bb152b003fcdf5d8e16240a763ed
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2023-02-21 11:21:34 +0200

    Update fulladd.v

--- Day1/fulladd.v
+++ Day1/fulladd.v
@@ -8,5 +8,5 @@ wire Sum_1, Carry_1, Carry_2;
 halfadd U1 (.Num_1(Num_1), .Num_2(Num_2), .Sum(Sum_1), .Carry(Carry_1));	
 halfadd U2 (.Num_1(Sum_1), .Num_2(C_in), .Sum(Sum), .Carry(Carry_2));
 or 		U3 (Carry, Carry_1, Carry_2);
-
+// assign carry = carry2 | carry1;
 endmodule
